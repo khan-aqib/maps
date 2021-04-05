3992963e8ef4a96d68567b2497809cee7258a552
khan-aqib <mujahidaqib1@gmail.com>
2021-04-06 01:08:13 +0400

    Updated index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.2/css/bulma.min.css">
+       
         <!-- Styles -->
         <style>
             html, body {
